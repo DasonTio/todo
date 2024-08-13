0c4b109455e52438fbc75ad72211dc814baaee5f
DasonTiovino <114289111+DasonTio@users.noreply.github.com>
2024-08-13 17:34:34 +0700

    Initial Commit

---
+++ todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by Dason Tiovino on 13/08/24.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
